5b7792f509a1b53d4ca4fdcfaadfcdaedbbad084
@hexlet/cli <support@hexlet.io>
2025-03-05 21:57:49 +0300

    submit java-web-ru/middlewares

---
+++ java-web-ru/middlewares/src/main/java/exercise/App.java
@@ -0,0 +1,66 @@
+package exercise;
+
+import io.javalin.Javalin;
+import exercise.controller.PostsController;
+import exercise.controller.RootController;
+import exercise.util.NamedRoutes;
+import io.javalin.rendering.template.JavalinJte;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+public final class App {
+
+    public static Javalin getApp() {
+
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
+        });
+
+        app.get(NamedRoutes.rootPath(), RootController::index);
+
+        app.get(NamedRoutes.buildPostPath(), PostsController::build);
+        app.post(NamedRoutes.postsPath(), PostsController::create);
+
+        app.get(NamedRoutes.postsPath(), PostsController::index);
+        app.get(NamedRoutes.postPath("{id}"), PostsController::show);
+
+        app.get(NamedRoutes.editPostPath("{id}"), PostsController::edit);
+        app.post(NamedRoutes.postPath("{id}"), PostsController::update);
+
+        // BEGIN
+        app.after(ctx -> {
+            if (ctx.result() != null) {
+                var xResponse = DigestUtils.sha256Hex(ctx.result());
+                ctx.header("X-Response-Digest", xResponse);
+            }
+            //MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            //byte[] encodedhash = digest.digest(
+            //        Objects.requireNonNull(ctx.result()).getBytes(StandardCharsets.UTF_8));
+            //String xResponse = bytesToHex(encodedhash);
+        });
+        // END
+
+        return app;
+    }
+
+    private static String bytesToHex(byte[] hash) {
+        StringBuilder hexString = new StringBuilder(2 * hash.length);
+        for (int i = 0; i < hash.length; i++) {
+            String hex = Integer.toHexString(0xff & hash[i]);
+            if(hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
+
+
+
